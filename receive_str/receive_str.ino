503b941b026a7a13289f2166471adf1f90504089
bear-KU <ueda2025@s.okayama-u.ac.jp>
2025-06-30 17:44:25 +0900

    Modify old version to diff

--- receive_str/receive_str.ino
+++ receive_str/receive_str.ino
@@ -51,7 +51,7 @@ void LED_read_binary(char *str) {
     if (threshold < value) {
       threshold = value;
     }
-    // Serial.printf("i: %d, value: %d, threshold: %d\r\n", i, value, threshold);
+    Serial.printf("i: %d, value: %d, threshold: %d\r\n", i, value, threshold);
     i++;
   }
   Serial.printf("Threshold after calibration: %d\r\n", threshold);
@@ -65,7 +65,7 @@ void LED_read_binary(char *str) {
     if (value < 0) {
       value = 0;
     }
-    // Serial.printf("prev_v: %d, value: %d\r\n", prev_v, value);
+    Serial.printf("prev_v: %d, value: %d\r\n", prev_v, value);
 
     // 終了判定
     if( (signal_unit != 0) &&                              // リーダを読んだ
@@ -82,7 +82,7 @@ void LED_read_binary(char *str) {
       // ノイズによる誤検出を防ぐ
       if((signal_unit * 0.7) < (time - edge_down_time)) {
         edge_up_time = time;
-        // Serial.printf("edge_up_time: %llf\r\n", edge_up_time);
+        Serial.printf("edge_up_time: %llf\r\n", edge_up_time);
       } 
     }
     // 2. 直前が High，現在が Low の場合(立ち下がり)
@@ -90,15 +90,15 @@ void LED_read_binary(char *str) {
       // ノイズによる誤検出を防ぐ -> TODO: ここはまだ実装していない
       
       edge_down_time = time;
-      // Serial.printf("edge_down_time: %lld\r\n", edge_down_time);
+      Serial.printf("edge_down_time: %lld\r\n", edge_down_time);
       
       // デコード
       signal_len = edge_down_time - edge_up_time;
       if(signal_unit == 0) {
         // 最初の信号(リーダ)の信号長を基本時間 T とする
         signal_unit = signal_len;
-        // Serial.printf("Read leader\r\n");
-        // Serial.printf("signal_unit: %lld\r\n", signal_unit);
+        Serial.printf("Read leader\r\n");
+        Serial.printf("signal_unit: %lld\r\n", signal_unit);
       }
       else {
         // ノイズによる誤検出を防ぐ
@@ -106,18 +106,18 @@ void LED_read_binary(char *str) {
           // データが0/1を判定
           if(signal_unit < signal_len * 0.6) {
             data |= 0x01;
-            // Serial.printf("1");
+            Serial.printf("1");
           }
           else {
             data |= 0x00;
-            // Serial.printf("0");
+            Serial.printf("0");
           }
           
           if(binary_len == 7) {
             // 1バイト分のデータが揃った
             str[str_len] = data;
             str_len++;
-            // Serial.printf(" --> '%c'\r\n", data);
+            Serial.printf(" --> '%c'\r\n", data);
 
             data = 0; // 次のデータ用に初期化
             binary_len = 0; // 次のデータ用に初期化
@@ -137,12 +137,10 @@ void LED_read_binary(char *str) {
     // 直前の値を更新
     prev_v = value;
 
-    // Serial.printf("edge_down_time: %lld, edge_up_time: %lld, signal_unit: %lld, signal_len: %lld\r\n", 
-    //   edge_down_time, edge_up_time, signal_unit, signal_len);
   } // while(1)
 } // LED_read_binary
 
-inline int get_light_intensity() {
+int get_light_intensity() {
   int intensity = 0;
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, 0);
