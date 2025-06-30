ea76f1904cf7d0d966b56fb65a06248c9e3c2666
bear-KU <ueda2025@s.okayama-u.ac.jp>
2025-06-30 17:46:09 +0900

    Update receive

--- receive_str/receive_str.ino
+++ receive_str/receive_str.ino
@@ -51,7 +51,6 @@ void LED_read_binary(char *str) {
     if (threshold < value) {
       threshold = value;
     }
-    Serial.printf("i: %d, value: %d, threshold: %d\r\n", i, value, threshold);
     i++;
   }
   Serial.printf("Threshold after calibration: %d\r\n", threshold);
@@ -65,7 +64,6 @@ void LED_read_binary(char *str) {
     if (value < 0) {
       value = 0;
     }
-    Serial.printf("prev_v: %d, value: %d\r\n", prev_v, value);
 
     // 終了判定
     if( (signal_unit != 0) &&                              // リーダを読んだ
@@ -82,7 +80,6 @@ void LED_read_binary(char *str) {
       // ノイズによる誤検出を防ぐ
       if((signal_unit * 0.7) < (time - edge_down_time)) {
         edge_up_time = time;
-        Serial.printf("edge_up_time: %llf\r\n", edge_up_time);
       } 
     }
     // 2. 直前が High，現在が Low の場合(立ち下がり)
@@ -90,15 +87,12 @@ void LED_read_binary(char *str) {
       // ノイズによる誤検出を防ぐ -> TODO: ここはまだ実装していない
       
       edge_down_time = time;
-      Serial.printf("edge_down_time: %lld\r\n", edge_down_time);
-      
+
       // デコード
       signal_len = edge_down_time - edge_up_time;
       if(signal_unit == 0) {
         // 最初の信号(リーダ)の信号長を基本時間 T とする
         signal_unit = signal_len;
-        Serial.printf("Read leader\r\n");
-        Serial.printf("signal_unit: %lld\r\n", signal_unit);
       }
       else {
         // ノイズによる誤検出を防ぐ
@@ -106,18 +100,15 @@ void LED_read_binary(char *str) {
           // データが0/1を判定
           if(signal_unit < signal_len * 0.6) {
             data |= 0x01;
-            Serial.printf("1");
           }
           else {
             data |= 0x00;
-            Serial.printf("0");
           }
           
           if(binary_len == 7) {
             // 1バイト分のデータが揃った
             str[str_len] = data;
             str_len++;
-            Serial.printf(" --> '%c'\r\n", data);
 
             data = 0; // 次のデータ用に初期化
             binary_len = 0; // 次のデータ用に初期化
@@ -140,7 +131,7 @@ void LED_read_binary(char *str) {
   } // while(1)
 } // LED_read_binary
 
-int get_light_intensity() {
+inline int get_light_intensity() {
   int intensity = 0;
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, 0);
